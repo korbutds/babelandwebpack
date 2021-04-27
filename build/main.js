e9f50497e9bf96bd81f774ded87766ca1a6cb09c
korbutds <sys@hohou.ru>
2021-04-27 13:55:49 +0300

    feat: Научимся создавать файл конфигурации webpack

--- build/main.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
-const App = () => /*#__PURE__*/React.createElement("h1", null, "Hello World");
-
-ReactDom.render( /*#__PURE__*/React.createElement(App, null), document.getElementById(`root`));
