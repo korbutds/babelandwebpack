b9c678ce403d672a5688c22c8dce7f3c6acf9cb0
korbutds <sys@hohou.ru>
2021-04-27 14:30:15 +0300

    feat: Научит понимать шрифты

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,28 @@ module.exports = {
     mode: "development",
 
     module: {
+        // Loader images
         rules: [
             {
-                test: /\.png$/,
+                test: /\.(png|jpg|gif|ico|jpeg)$/,
                 use: [
                     {
                         loader: 'file-loader',
                         options: {
                             outputPath: 'images',
+                            name: '[name]-[sha1:hash:7].[ext]'
+                        }
+                    }   
+                ]
+            },
+            //Loader fonts
+            {
+                test: /\.(ttf|otf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'fonts',
                             name: '[name]-[sha1:hash:7].[ext]'
                         }
                     }   
